7003175a9e5dbe344b6f54bbd69490307d144890
Hunter <huntercbaldwin98@gmail.com>
2022-01-28 10:19:35 -0800

    Added basic registration and login functionality

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseAPIController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+            
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            // call our helper function that determines whether a user already exists with 
+            // the desired username and returns a BadRequest, which tells the user their problem.
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+            // randomly generates a SHA512 key 
+            using var hmac = new HMACSHA512();
+
+            // creating our new user. The password hash is the entered password 
+            // computed with our hmac var previously.
+            // we first must encode our string password into bytes
+            // before computing the hash
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            // prepares the new user to be added to our database
+            _context.Users.Add(user);
+
+            // this will save the prepared new user to our database
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        [HttpPost("Login")]
+
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+            
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+    }
+}
