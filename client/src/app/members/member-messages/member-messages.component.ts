506bc44a23c9e25bc35aca7b80f9a4960fb9f22c
Hunter <huntercbaldwin98@gmail.com>
2022-03-03 13:49:42 -0800

    Applied fix

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,7 +1,6 @@
 import { MessageService } from './../../_services/message.service';
 import { Message } from './../../_models/message';
 import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
-import { User } from 'src/app/_models/user';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -15,6 +14,7 @@ export class MemberMessagesComponent implements OnInit {
   @Input() username: string;
   @ViewChild('messageForm') messagForm: NgForm;
   messageContent: string;
+  loading = false;
 
   constructor(public messageService: MessageService) { }
 
@@ -22,9 +22,10 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   sendMessage() {
+    this.loading = true;
     this.messageService.sendMessage(this.username, this.messageContent).then(() => {
       this.messagForm.reset();
-    });
+    }).finally(() => this.loading = false);
   }
 
 }
